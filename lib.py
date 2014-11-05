0d2f798ff1096f14e5a65928c0b5e8862dbd8c4a
Hackbright Student <info@hackbrightacademy.com>
2014-11-04 18:17:56 -0800

    Yay mentors!

--- lib.py
+++ lib.py
@@ -1,7 +1,16 @@
+from flask import Flask 
 import eventbrite 
 import os
 
-def apicall(maxresults = 100, page = 1):
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return apicall()
+
+def apicall(maxresults = 10, page = 1):
+    if page > 1:
+        return
     auth_tokens = {'app_key':  os.environ['app_key'],
                       'user_key': os.environ['user_key']}
     client = eventbrite.EventbriteClient(auth_tokens)
@@ -11,21 +20,25 @@ def apicall(maxresults = 100, page = 1):
 
     #response=client.eventsearch({'q':"dance"})
     response = client.event_search({"city":"San Francisco","category":"music", "max": maxresults, "page": page})
-    
+    rendered_events = ''
     #print response
     events = response['events']
     for i in range(len(events)):
         if "event" in events[i]:
             event = events[i]["event"]
-            print event['title'],event['id']
-            if "tickets" in event:
-                tickets = event["tickets"]
-                for j in range(len(tickets)):
-                    pass
-                    #print tickets[j]["ticket"]
-                #pass
-            #print events[i]["event"]["title"]
-            #print "\n"
+            
+            row = [event['title'],str(event['id'])]
+            rendered_events += ",".join(row)
+
+            print "\n"
+            # if "tickets" in event:
+            #     tickets = event["tickets"]
+            #     for j in range(len(tickets)):
+                    
+            #         print tickets[j]["ticket"]
+            #     #pass
+            # # print events[i]["event"]["title"]
+            # print "\n"
         else :
             total_items = events[i]["summary"]["total_items"]
             if (maxresults * page) < total_items:
@@ -35,5 +48,9 @@ def apicall(maxresults = 100, page = 1):
             else:
                 print "GOT ALL THE STUFF!"
 
+    return rendered_events
 
-apicall()
+# apicall()
+
+if __name__=="__main__":
+    app.run(debug=True)
