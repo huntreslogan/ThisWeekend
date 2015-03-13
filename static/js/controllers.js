4aba5ce8c42f62be96002a6360f4bc568ce4a580
Jessica Hunt <huntres@me.com>
2015-03-13 13:25:31 -0700

    Added comments in controller

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -2,35 +2,28 @@
 
 var eventControllers = angular.module('eventControllers', []);
 
-// eventControllers.controller('EventListCtrl', ['$scope', 'Event',
-//   function($scope, Event) {
-//     $scope.events = Event.query();
-//     $scope.orderProp = 'title';
-//   }]);
-
-// eventControllers.controller('EventDetailCtrl', ['$scope', '$routeParams', 'Event',
-//   function($scope, $routeParams, Event) {
-//     $scope.event = Event.get({eventId: $routeParams.eventId}, function(event) {
-//       $scope.mainImageUrl = event.image;
-//     });
-
-//     $scope.setImage = function(image) {
-//       $scope.mainImageUrl = image;
-//     };
-//   }]);
+//Create controller module to request data from the server for each event in database
+//Sets data equal to variable events and binds events to the scope
+//Sets orderProp equal to date so that we can repeat our events in the event list template with ng-repeat and order by the attribute date
 
 eventControllers.controller('EventListCtrl', ['$scope', '$http', "currentUser",
   function($scope, $http, currentUser) {
-    console.log("in events controller");
     $http.get('/api/events').success(function(data) {
       $scope.events = data;
     });
 
     $scope.orderProp = 'date';
 
-    // alert(currentUser.username);
-
   }]);
+
+
+//Creates controller for our event details and uses the event Id parameter to request data from the server
+//The variable eventDetail is set equal to the data returned from the server and can be used in our event detail view
+//The save function posts the event being viewed by the user to the server
+//The server then places the event in an association table created to hold each user's saved events for later viewing
+//The events can be accessed by selecting the saved events link in the navbar which displays the user's saved events in a drop-down modal
+//Share allows the user to share the event with other user's by submiting their username, which causes a post to server and the event is placed into that user's saved events table
+
 
 eventControllers.controller('EventDetailCtrl', ['$scope', '$routeParams', '$http', 'currentUser',
   function($scope, $routeParams, $http, currentUser) {
@@ -39,45 +32,35 @@ eventControllers.controller('EventDetailCtrl', ['$scope', '$routeParams', '$http
     $http.get(
       '/api/eventdetail/' + $routeParams.eventId
       ).success(function(data) {
-        console.log(data);
         $scope.eventdetail = data;
         $scope.save = function(e) {
-          // console.log(e);
           $http.post('/savedevent', e, currentUser).success (function(data) {
-            console.log("Awesome " + data + currentUser);
 
         })}
 
         $scope.share = function(o) {
             var eventdetail = $scope.eventdetail;
-            console.log(o);
-            console.log(eventdetail);
-            console.log([eventdetail, o]);
             var sharethis = [eventdetail,o];
             $http.post('/shareit', sharethis).success(function(data) {
-            console.log('Share some' + data);
           })
         }
       });
 
   }]);
 
+//Once the user submits their username and email the data is posted to the server and saved in a database table named users
+
 eventControllers.controller('SignupCtrl', ['$scope', '$http',
   function ($scope, $http) {
-  console.log("in home");
 
   $scope.reset = function (u) {
     $scope.user = {};
-    console.log("huh?");
   };
 
   $scope.submit = function(u) {
-    // alert("Hix " + u.username);
     $http.post('/submituser', u).success(
          function(data) {
-         console.log("Flask said " + data);
-         // self.wrongpassword = true;
-
+         alert("Welcome!")
        }
       );
 
@@ -88,27 +71,19 @@ eventControllers.controller('SignupCtrl', ['$scope', '$http',
 
 ]);
 
-// eventControllers.controller('HomeCtrl', [function() {
-//   window.location.href = 'http://google.com';
-// }] );
+
 
 eventControllers.controller('LoginCtrl', ['$scope', '$http', 'currentUser',
   function ($scope, $http, currentUser){
-
-    console.log('in login');
-
-    // $scope.currentUser = currentUser;
-    // $scope.currentUser = {'username': ''}
 
   $scope.login = function (u) {
 
     $http.post('/login', u).success(
       function(data) {
-        console.log("login");
         if (data == "Please enter correct password") {
-         console.log("Needs correct password");
+         alert("Please submit correct password");
         }else {
-          console.log("Got correct password");
+          alert("Welcome" + $scope.currentUser.username);
           $scope.currentUser.username = u.username;
         }
       }
@@ -123,14 +98,10 @@ eventControllers.controller('savedEventCtrl', ['$scope', '$http',
   function($scope, $http) {
       $scope.savedevents = [];
       $http.get('/modalevents').success (function (data) {
-        console.log("in savedEventCtrl");
-        console.log(data);
         var savedevents = data;
         $scope.savedevents = savedevents;
-        console.log(savedevents);
         var firstevent = savedevents[0];
         $scope.firstevent = firstevent;
-        console.log(firstevent.image);
         var image = firstevent.image;
         $scope.image = image;
 
